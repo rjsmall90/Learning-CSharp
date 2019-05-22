c84b5bb46dd8f4a1fee3d697cdce6ce08dc0022f
rjsmall90 <rjsmall90@gmail.com>
2019-05-22 16:59:41 -0400

    practicing ifs and loops

---
+++ Loops_And_Ifs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loops_And_Ifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+        }
+    }
+}
